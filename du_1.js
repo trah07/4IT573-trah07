4ae3cf7522cacf004f9e860ab24bc62006865fe9
trah07 <trah07@vse.cz>
2024-02-19 17:52:41 +0100

    Změna rl.question() na prompt()

--- du_1.js
+++ du_1.js
@@ -1,34 +1,23 @@
-const readline = require('readline');
-
 const randomNum = Math.floor(Math.random() * 10) + 1;
-
 const maxAttempts = 3;
 let attempts = 0;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 function askUser() {
-  rl.question('Uhádni číslo od 1 do 10: ', (userInput) => {
-    const userGuess = parseInt(userInput);
+  const userInput = prompt('Uhádni číslo od 1 do 10: ');
+  const userGuess = parseInt(userInput);
 
-    if (userGuess === randomNum) {
-      console.log('Gratuluji! Uhádl jsi číslo.');
-      rl.close();
+  if (userGuess === randomNum) {
+    console.log('Gratuluji! Uhádl jsi číslo.');
+  } else {
+    console.log('Špatně. Zkus to znovu.');
+    attempts++;
+
+    if (attempts < maxAttempts) {
+      askUser();
     } else {
-      console.log('Špatně. Zkus to znovu.');
-      attempts++;
-
-      if (attempts < maxAttempts) {
-        askUser();
-      } else {
-        console.log(`Bohužel jsi nepřesáhl maximální počet pokusů. Hledané číslo bylo: ${randomNum}`);
-        rl.close();
-      }
+      console.log(`Bohužel jsi nepřesáhl maximální počet pokusů. Hledané číslo bylo: ${randomNum}`);
     }
-  });
+  }
 }
 
 askUser();
