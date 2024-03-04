66a0c5e3448b1e4655ccb2ab6fe0b18014a7e9f6
trah07 <trah07@vse.cz>
2024-03-04 17:19:56 +0100

    Oprava kódu

--- du_3/index.mjs
+++ du_3/index.mjs
@@ -18,20 +18,16 @@ async function createFiles(num) {
 
 async function main() {
   try {
-    // Přečtení obsahu souboru "instrukce.txt"
     const instructionContent = await fs.readFile('instrukce.txt', 'utf-8');
     const num = parseInt(instructionContent);
 
-    // Kontrola, zda se jedná o platné číslo
     if (isNaN(num)) {
       console.error('Chyba: Obsah souboru "instrukce.txt" není platné číslo.');
       return;
     }
 
-    // Vytvoření souborů paralelně pomocí Promise.all
     await createFiles(num);
 
-    // Informativní hláška
     console.log('Všechny soubory byly úspěšně vytvořeny.');
 
   } catch (error) {
@@ -39,5 +35,4 @@ async function main() {
   }
 }
 
-// Spuštění hlavní funkce
 main();
